ca4636a3d4d7c729ebab36985a641628a4819489
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-17 18:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+
+
+const driver = {
+  
+}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  const newUpdateDriver = {...driver};
+  newUpdateDriver [key] = value;
+  return newUpdateDriver;
+}
+
